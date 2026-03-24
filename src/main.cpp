d6ef7a48507f3e53a9d3bc7fcb04e55af74cbfef
Moe <muhtasim1023@gmail.com>
2026-03-23 20:08:23 -0400

    setpoint matrix added to set differnt x setpoints

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main() {
   myFile << "Time,Pos_X,Angle\n";
 
   //Simulate
-  const float duration {12.f};
+  const float duration {10.f};
   float time{0.f};
   float dt{0.001f};
 
@@ -30,28 +30,33 @@ int main() {
   Eigen::Vector4f B;
   Eigen::RowVector4f K;
   float u;
+  Eigen::Vector4f setpoint;
 
+  // This is the linearized state
+  // TODO: Need Non-Linear Model
   A << 0.f, 1.f, 0.f, 0.f, 
-          0.f, 0.f, -m*g/M, 0.f, 
-          0.f, 0.f, 0.f, 1.f, 
-          0.f, 0.f, (M + m)*g/(M*l), 0.f;
+       0.f, 0.f, -m*g/M, 0.f, 
+       0.f, 0.f, 0.f, 1.f, 
+       0.f, 0.f, (M + m)*g/(M*l), 0.f;
 
   x << 0.f, 0.f, .5f, 0.f;
 
-  x_dot << 0.f, 0.f, 0.f, 0.f;
+  x_dot << 1.f, 0.f, 0.f, 0.f;
 
   B << 0.f, 1.f/M, 0.f, -1.f/(M*l);
 
   //K << -1.3003f,-2.3173f,-26.6124f,-5.6587f;
   //K << -874.92f,-307.24f,-843.75f,-176.62f;
-  //K << -12.237f,-20.598f,-110.906f,-22.799f;
+  K << -12.237f,-20.598f,-110.906f,-22.799f;
   //K << -0.1225,-0.3952,-15.5213,-2.6976;
+
+  setpoint << 0, 0, 0, 0;
 
   while(time < duration){
     
     myFile << time << "," << x(0) << "," << x(2) << "\n";
 
-    u = -(K * x)(0);
+    u = -(K * (x - setpoint))(0);
     x_dot = (A * x) + (B * u);
     x += x_dot * dt;
     time += dt;
