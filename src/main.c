fc4657e11eeab13b3df0505a8fe6af8fce4a9a09
Moe <muhtasim1023@gmail.com>
2026-04-08 23:27:19 -0400

    updated pole length variable name l->L

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main(){
 
   printf("%lf,%lf,%lf,%lf\n", pendulum_params.g, 
-    pendulum_params.m, pendulum_params.M, pendulum_params.l);
+    pendulum_params.m, pendulum_params.M, pendulum_params.L);
 
   printf("%lf,%lf,%lf,%lf\n", motor_params.k1, 
     motor_params.k2, motor_params.R, motor_params.r);
