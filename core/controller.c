786d7e5eee08cf31915ae32e41a953b8772aff31
Moe <muhtasim1023@gmail.com>
2026-04-09 22:43:36 -0400

    just realized how stupid the switch case looked so I opted to use a data table

--- core/controller.c
+++ core/controller.c
@@ -9,43 +9,14 @@
 
 void gain_settings(control_t control_mode, gain_t* gain){
 
-  switch(control_mode){
-    case GENTLE:
-      gain->A = -1.3003;
-      gain->B = -2.3173;
-      gain->C = -26.6124;
-      gain->D = -5.6587;
-      break;
-    case AGRESSIVE:
-      gain->A = -2.5390;
-      gain->B = -3.9352;
-      gain->C = -34.1295;
-      gain->D = -7.4676;
-      break;
-    case OPTIMAL:
-      gain->A = -10.000;
-      gain->B = -23.712;
-      gain->C = -237.411;
-      gain->D = -113.135;
-      break;
-    case GENTLE_DC:
-      gain->A = -0.1225;
-      gain->B = -1.3951;
-      gain->C = -15.5235;
-      gain->D = -2.6976;
-      break;
-    case AGRESSIVE_DC:
-      gain->A = -9.0346;
-      gain->B = -10.9102;
-      gain->C = -55.2573;
-      gain->D =-12.2551;
-      break;
-    case OPTIMAL_DC:
-      gain->A = -10.000;
-      gain->B = -10.267;
-      gain->C = -52.147;
-      gain->D = -11.563;
-      break;
-  }
+  static const gain_t GAIN_TABLE[] = {
+    [GENTLE]       = {-1.3003, -2.3173, -26.6124, -5.65870},
+    [AGRESSIVE]    = {-2.5390, -3.9352, -34.1295, -7.46760},
+    [OPTIMAL]      = {-10.000, -23.712, -237.411, -113.135},
+    [GENTLE_DC]    = {-0.1225, -1.3951, -15.5235, -2.69760},
+    [AGRESSIVE_DC] = {-9.0346, -10.910, -55.2573, -12.2551},
+    [OPTIMAL_DC]   = {-10.000, -10.267, -52.1470, -11.5630}
+  };
 
+  *gain = GAIN_TABLE[control_mode];
 }
