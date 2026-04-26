fdded6254618ca27e5b0e66b8a9a4ba83622cad5
Moe <muhtasim1023@gmail.com>
2026-04-25 22:14:40 -0400

    simulated inverted pendulum with noise and step input response

--- build-run-plot.sh
+++ build-run-plot.sh
@@ -8,10 +8,19 @@ gnuplot -p -e " set grid;
                 set key tc rgb 'white';
                 set terminal x11 0 title "Simulation" persist;
                 set object 1 rectangle from screen 0,0 to screen 1,1 fillcolor rgb 'black' behind;
-                plot 'pendulum_sim.dat' using 1:2 w l title 'X-Position', ''
+                plot 'pendulum_sim.csv' using 1:2 w l title 'X-Position', ''
                 using 1:3 w l title 'Velocity', ''
                 using 1:4 w l title 'Pole Angle', '' 
                 using 1:6 w l title 'Position Setpoint'"
+
+gnuplot -p -e " set grid; 
+                set datafile separator ','; 
+                set grid lc rgb 'white';
+                set border lc rgb 'white';
+                set key tc rgb 'white';
+                set terminal x11 0 title "Noise" persist;
+                set object 1 rectangle from screen 0,0 to screen 1,1 fillcolor rgb 'black' behind;
+                plot 'pendulum_sim.csv' using 1:7 title 'Noise'"
 
 gnuplot -p -e " set grid; 
                 set datafile separator ',';
@@ -20,4 +29,4 @@ gnuplot -p -e " set grid;
                 set key tc rgb 'white';
                 set terminal x11 0 title "Force" persist;
                 set object 1 rectangle from screen 0,0 to screen 1,1 fillcolor rgb 'black' behind; 
-                plot 'pendulum_sim.dat' using 1:5 w l title 'Input Force - [N]'"
+                plot 'pendulum_sim.csv' using 1:5 w l title 'Input Force - [N]'"
--- src/main.c
+++ src/main.c
@@ -1,19 +1,22 @@
 #include "controller.h"
 #include "estimator.h"
 #include "physics.h"
-#include <math.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(){
 
-  FILE *fpt = fopen("pendulum_sim.dat", "w");
+  //Generate seed for Guassian noise generation
+  srand(time(NULL));
+
+  FILE *fpt = fopen("pendulum_sim.csv", "w");
 
   if (fpt == NULL) {
       fprintf(stderr, "Error opening file!\n");
       return 1;
   }
 
-  const double duration = 15; // Units [sec]
+  const double duration = 10; // Units [sec]
   double time = 0;            // Units [sec]
   double dt = 0.01;           // Units [sec]
 
@@ -28,25 +31,23 @@ int main(){
 
   while(time < duration){
 
-    //step function
-    if((time > 3) && (time < 6)){
-      setpoint.pendulum.x = 1;
-    }else if((time > 6) && (time < 9)){
-      setpoint.pendulum.x = 0;
-    }else if((time > 9) && (time < 12)){
-      setpoint.pendulum.x = -1;
-    }else if((time > 12)){
-      setpoint.pendulum.x = 0;
-    }
+    state_t noise = {gaussian_generator(0, 0.004),   //State Process Noise
+                     gaussian_generator(0, 0.005),  //State Process Noise
+                     gaussian_generator(0, 0.002),   //State Process Noise
+                     gaussian_generator(0, 0.005)};//State Process Noise
+
+    setpoint.pendulum.x = sin(9*time/5);
 
     for(size_t i = 0; i < 4; ++i){
+      x.arr[i] = x.arr[i] + noise.arr[i]; // Add process noise to current state
       u -= K[i]*(x.arr[i] - setpoint.arr[i]);
     }
 
     state_t next_state = {0,0,0,0};
     rk4_step(&x, &next_state, pendulum_params, u, dt);
 
-    fprintf(fpt, "%lf,%lf,%lf,%lf,%lf,%lf\n", time, x.pendulum.x, x.pendulum.x_dot, x.pendulum.theta, u, setpoint.pendulum.x);
+    fprintf(fpt, "%lf,%lf,%lf,%lf,%lf,%lf\n", time, 
+      x.pendulum.x, x.pendulum.x_dot, x.pendulum.theta, u, setpoint.pendulum.x);
 
 
     x = next_state;
