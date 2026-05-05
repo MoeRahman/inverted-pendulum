8db1da57dddea77dec875536b51854e7692b515f
Moe <muhtasim1023@gmail.com>
2026-05-05 17:20:43 -0400

    added measurement noise to sensor measurement

--- core/physics.c
+++ core/physics.c
@@ -8,8 +8,19 @@
 - ODE Solver
 */
 
-const pendulum_params_t pendulum_params = {.G = 9.80665, .m = 10, .M = 20, .L = 0.5, .b = 2, .g = 1};
-const motor_params_t motor_params = {.k1 = 1, .k2 = 1, .R  = 1, .r  = 1};
+const pendulum_params_t pendulum_params = {
+  .G = 9.80665, 
+  .m = 10, 
+  .M = 20, 
+  .L = 0.5, 
+  .b = 2, 
+  .g = 1};
+  
+const motor_params_t motor_params = {
+  .k1 = 1, 
+  .k2 = 1, 
+  .R  = 1, 
+  .r  = 1};
 
 double gaussian_generator(double mean, double std_dev){
 
@@ -33,10 +44,10 @@ void pendulum_dynamics(vect4d_t const *curr_state, vect4d_t* next_state,
     }
 
     //pendulum state variables
-    const double x          = curr_state->pendulum.x;
-    const double x_dot      = curr_state->pendulum.x_dot;
-    const double theta      = curr_state->pendulum.theta;
-    const double theta_dot  = curr_state->pendulum.theta_dot;
+    const double x          = curr_state->state.x;
+    const double x_dot      = curr_state->state.x_dot;
+    const double theta      = curr_state->state.theta;
+    const double theta_dot  = curr_state->state.theta_dot;
 
     //pendulum params
     const double G = pendulum_params.G;
--- include/physics.h
+++ include/physics.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "types.h"
 #include <math.h>
 #include <stdbool.h>
 #include <time.h>
@@ -10,36 +11,6 @@
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
-
-typedef struct{
-  const double G; //gravity     [m/s^2]
-  const double m; //ball mass   [kg]
-  const double M; //cart mass   [kg]
-  const double L; //pole length [m]
-  const double b; //linear viscous damping      [N*s/m]
-  const double g; //rotational viscous damping  [N*m*s/rad]
-}pendulum_params_t;
-
-
-typedef struct{
-  const double k1; //torque constant           [N*m/A]
-  const double k2; //back-emf constant         [V/kRPM]
-  const double R;  //motor internal resistance [Ω]
-  const double r;  //torque to force ratio
-}motor_params_t;
-
-
-typedef union{
-  double arr[4];
-
-  struct{
-    double x;         //position         [m]
-    double x_dot;     //velocity         [m/s]
-    double theta;     //pole angle       [rad]
-    double theta_dot; //angular velocity [rad/s]
-  }pendulum;
-
-}vect4d_t;
 
 
 extern const pendulum_params_t pendulum_params;
