075ea3ef90803cbb20993a56315a9fedf1da4bdf
Moe <muhtasim1023@gmail.com>
2026-05-23 21:36:41 -0400

    passed kalman filter pointer rather than reference to pointer

--- src/main.c
+++ src/main.c
@@ -24,12 +24,12 @@ int main(){
   double dt = 0.01; //Units [sec]
 
   //Write column titles
-  fprintf(fpt, "Time,Pos_X,Vel_X,Angle,Force,Setpoint,ERROR\n");
+  fprintf(fpt, "i.time,ii.Pos_X,iii.vel_X,iv.angle,v.force,vi.setpoint,vii.error,viii.pos_est,ix.vel_est,x.angle_est\n");
 
-  vect4d_t x = {0,0,-1e-3,0};          //State {m, m/s, rad, rad/s}
+  vect4d_t state = {0,0,-1e-3,0};      //State {m, m/s, rad, rad/s}
   vect4d_t next_state = {0,0,0,0};     //Next State
 
-  vect4d_t x_est = {0,0,0,0};          //State Estimate
+  vect4d_t state_est = {0,0,0,0};      //State Estimate
   vect4d_t dx_est = {0,0,0,0};         // d/dt (State Estimate)
 
   double y = 0.0;                      //Position Measurement
@@ -41,8 +41,7 @@ int main(){
 
   //State Process Noise
   vect4d_t noise = {0,0,0,0};
-  const double noise_variance[4] = {POS_NOISE, VEL_NOISE, 
-                                    ANGLE_NOISE, OMEGA_NOISE};
+  const double noise_variance[4] = {POS_NOISE, VEL_NOISE, ANGLE_NOISE, OMEGA_NOISE};
 
   //Measurement Noise
   double sensor_noise = 0;
@@ -55,37 +54,33 @@ int main(){
     for(size_t i = 0; i < 4; ++i){
       noise.arr[i] = gaussian_generator(0, noise_variance[i]);
     }
+
     sensor_noise = gaussian_generator(0, POS_SENSOR_NOISE);
 
-    if((time >= 2) && (time < 4))setpoint.state.x = 0.1;
+    if((time >= 2) && (time < 4))setpoint.state.x = 1;
 
     //Measure Position
-    y = x.state.x + sensor_noise;
+    y = state.state.x + sensor_noise;
 
     //Full-State Estimation
-    //kalman_filter(&x_est, &dx_est, u, y, &Kf);
-    rk4_step(kalman_filter, &x_est, &dx_est, u, y, &Kf, dt);
+    rk4_step(kalman_filter, &state_est, &state_est, u, y, Kf, dt);
 
+    u = 0;
     for(size_t i = 0; i < 4; ++i){
-      x_est.arr[i] = dx_est.arr[i];
-      u -= Kc[i]*(x.arr[i] - setpoint.arr[i]);
+      u -= Kc[i]*(state.arr[i] - setpoint.arr[i]);
     }
 
-    dx_est = (vect4d_t){0,0,0,0};
+    rk4_step(pendulum_dynamics, &state, &next_state, u, y, &Kf, dt);
 
-    rk4_step(pendulum_dynamics, &x, &next_state, u, y, &Kf, dt);
-
-    fprintf(fpt, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf\n", 
-            time, x.state.x, x.state.x_dot, x.state.theta, u, 
-            setpoint.state.x, setpoint.state.x - x.state.x,
-            x_est.state.x, x_est.state.x_dot, x_est.state.theta);
+    fprintf(fpt, "%lf,%lf,%lf,%lf,%lf,", time, state.state.x, state.state.x_dot, state.state.theta, u/1000); 
+    fprintf(fpt, "%lf,%lf,", setpoint.state.x, setpoint.state.x - state.state.x);
+    fprintf(fpt, "%lf,%lf,%lf,%lf\n", state_est.state.x, state_est.state.x_dot, state_est.state.theta, y);
 
     for(size_t i = 0; i < 4; ++i){
-      x.arr[i] = next_state.arr[i] + noise.arr[i];
+      state.arr[i] = next_state.arr[i] + noise.arr[i];
     }
 
     next_state = (vect4d_t){0,0,0,0};
-    u = 0;
     time += dt;
   }
 
