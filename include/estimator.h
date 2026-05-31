960a38c77814bbad39268ce8ad337d67db041eda
Moe <muhtasim1023@gmail.com>
2026-05-31 18:16:26 -0400

    update sensor noise covar

--- include/estimator.h
+++ include/estimator.h
@@ -4,13 +4,13 @@
 #include "stddef.h"
 
 //Process Noise
-#define POS_NOISE   1e-4
-#define VEL_NOISE   1e-3
-#define ANGLE_NOISE 1e-5
-#define OMEGA_NOISE 1e-4
+#define POS_NOISE   0
+#define VEL_NOISE   1e-7
+#define ANGLE_NOISE 0
+#define OMEGA_NOISE 1e-6
 
 //Sensor Noise
-#define POS_SENSOR_NOISE   1e-3
+#define POS_SENSOR_NOISE   1e-5
 #define ANGLE_SENSOR_NOISE 1e-3
 
 double angular_encoder_sensor(double theta_measurement);
