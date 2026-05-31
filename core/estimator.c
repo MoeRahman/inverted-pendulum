78c8f4117c8562590b53ecc81fc05f55fb4ef20a
Moe <muhtasim1023@gmail.com>
2026-05-31 17:13:10 -0400

    removed unused libraries and updated linear system for LQE

--- core/estimator.c
+++ core/estimator.c
@@ -1,7 +1,6 @@
 #include "estimator.h"
 #include "parameters.h"
-#include <string.h>
-#include <stdio.h>
+
 /*
 - Optimal Linear State Observer
 - Kalman Filter
@@ -23,34 +22,48 @@ void kalman_filter(vect4d_t* state_estimate,
         d_dt_state_estimate->arr[i] = 0.0;
     }
     
-    double a22 = pendulum_params.b/pendulum_params.M;
-    double a23 = (pendulum_params.m*pendulum_params.g)/pendulum_params.M;
-    double a24 = pendulum_params.gamma/pendulum_params.M;
-    double a42 = pendulum_params.b/(pendulum_params.M*pendulum_params.L);
-    double a43 = ((pendulum_params.m + pendulum_params.M)/(pendulum_params.M*pendulum_params.L))*pendulum_params.g;
-    double a44 = a43*((pendulum_params.gamma)/(pendulum_params.m*pendulum_params.L*pendulum_params.g));
+    double g     = pendulum_params.g;
+    double m     = pendulum_params.m;
+    double M     = pendulum_params.M;
+    double l     = pendulum_params.L;
+    double b     = pendulum_params.b;
+    double gamma = pendulum_params.gamma;
 
-    //State Transition Matrix
+    double mt = M + m;
+    double I  = (1.0 / 3.0) * m * l * l;
+
+    double den = 4.0 * I * mt - l * l * m * m + l * l * m * mt;
+
+    double a22 = (-4.0 * b * (I + (l * l * m) / 4.0)) / den;
+    double a23 = (-g * l * l * m * m) / den;
+    double a24 = (2.0 * gamma * l * m) / den;
+    
+    double a42 = (2.0 * b * l * m) / den;
+    double a43 = (2.0 * g * l * m * mt) / den;
+    double a44 = (-4.0 * gamma * mt) / den;
+
     double A[4][4] = {
-        {0,    1,    0,    0},
-        {0, -a22,  -a23, a24},
-        {0,    0,    0,    1},
-        {0,  a42,  a43, -a44}
+        {0.0, 1.0, 0.0, 0.0},
+        {0.0, a22, a23, a24},
+        {0.0, 0.0, 0.0, 1.0},
+        {0.0, a42, a43, a44}
     };
 
-    //Input Matrix
-    double B[4] = {0, 1/pendulum_params.M, 0, -1/(pendulum_params.M*pendulum_params.L)};
+    double B[4] = {
+        0.0,
+        (4.0 * (I + (l * l * m) / 4.0)) / den,
+        0.0,
+        (-2.0 * l * m) / den
+    };
 
-    //Measurement Matrix
-    double C[4] = {1, 0, 0, 0};
-    double obsvr_error = measurement - C[0]*state_estimate->arr[0];
+    double obsvr_error = measurement - state_estimate->arr[0];
 
-    // d/dt x_est = A*x_est + B*u + Kf*(y - C*x_est)
+    //d/dt x_est = A*x_est + B*u + Kf*(y - C*x_est)
     for(size_t i = 0; i < 4; ++i){
-      for(size_t j = 0; j < 4; ++j){
-        d_dt_state_estimate->arr[i] += A[i][j]*state_estimate->arr[j];
-      }
-      d_dt_state_estimate->arr[i] +=  B[i]*input + kalman_gain[i]*obsvr_error;
+        for(size_t j = 0; j < 4; ++j){
+            d_dt_state_estimate->arr[i] += A[i][j] * state_estimate->arr[j];
+        }
+        d_dt_state_estimate->arr[i] += B[i] * input + kalman_gain[i] * obsvr_error;
     }
 }
 
@@ -58,7 +71,7 @@ void kalman_filter(vect4d_t* state_estimate,
 double* set_estimator_gain(gain_t gain){
 
   static double GAIN_TABLE[3][4] = {
-    {10.236, 52.391, -58.799, -300.807}, //theta = 0
+    {12.435, 77.262, -171.370, -944.128}, //theta = 0
     {6.8046e-01, 2.2651e-01, -1.7617e-03, 5.0846e-05}, //theta = M_PI
     {6.8278e-03, 1.8618e-03, 1.2109e-05, 3.4978e-04}
   };
