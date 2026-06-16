e7931ff15c93a1f43b5b4672ec4da4c69be69818
Moe <muhtasim1023@gmail.com>
2026-06-16 12:04:30 -0400

    moved A and B matrix out of kalman function

--- core/estimator.c
+++ core/estimator.c
@@ -21,40 +21,6 @@ void kalman_filter(vect4d_t* state_estimate,
     for(size_t i = 0; i < 4; ++i) {
         d_dt_state_estimate->arr[i] = 0.0;
     }
-    
-    double g     = pendulum_params.g;
-    double m     = pendulum_params.m;
-    double M     = pendulum_params.M;
-    double l     = pendulum_params.L;
-    double b     = pendulum_params.b;
-    double gamma = pendulum_params.gamma;
-
-    double mt = M + m;
-    double I  = (1.0 / 3.0) * m * l * l;
-
-    double den = 4.0 * I * mt - l * l * m * m + l * l * m * mt;
-
-    double a22 = (-4.0 * b * (I + (l * l * m) / 4.0)) / den;
-    double a23 = (-g * l * l * m * m) / den;
-    double a24 = (2.0 * gamma * l * m) / den;
-    
-    double a42 = (2.0 * b * l * m) / den;
-    double a43 = (2.0 * g * l * m * mt) / den;
-    double a44 = (-4.0 * gamma * mt) / den;
-
-    double A[4][4] = {
-        {0.0, 1.0, 0.0, 0.0},
-        {0.0, a22, a23, a24},
-        {0.0, 0.0, 0.0, 1.0},
-        {0.0, a42, a43, a44}
-    };
-
-    double B[4] = {
-        0.0,
-        (4.0 * (I + (l * l * m) / 4.0)) / den,
-        0.0,
-        (-2.0 * l * m) / den
-    };
 
     double obsvr_error = measurement - state_estimate->arr[0];
 
